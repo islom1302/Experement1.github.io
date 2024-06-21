936c4efd78da60a574c8a6d3ea604590bbb2af4c
islom1302 <aalita004@gmail.com>
2024-06-21 15:06:19 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
-function showTime() {
-	document.getElementById('currentTime').innerHTML = new Date().toUTCString();
-}
-showTime();
-setInterval(function () {
-	showTime();
-}, 1000);
+document.querySelector('form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    const form = event.target;
+    const data = new FormData(form);
+
+    fetch('/', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+        body: new URLSearchParams(data).toString()
+    })
+    .then(response => response.json())
+    .then(data => {
+        document.getElementById('response').innerHTML = '<p>Form successfully submitted!</p>';
+        form.reset();
+    })
+    .catch(error => console.error('Form submission error:', error));
+});
